bb5ab6e530103d611abb594ec30190903a756919
E07ZK <ef22dec@icloud.com>
2019-11-13 21:17:48 +0900

    create loginAuthen

--- app/controllers/ishiharas_controller.rb
+++ app/controllers/ishiharas_controller.rb
@@ -2,15 +2,27 @@ class IshiharasController < ApplicationController
   def index
   end
 
+  def login
+  end
+
+  def loginAuthen
+  end
+
   def step1
   end
 
   def step2
   end
 
+  def step2a
+  end
+
   def step3
   end
 
   def step4
+  end
+
+  def step5
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   root 'ishiharas#index'
+  get '/ishiharas/login', to: "ishiharas#login"
+  get '/ishiharas/loginAuthen', to: "ishiharas#loginAuthen"
 
   resources :ishiharas do
     collection do
       get 'step1'
       get 'step2'
+      get 'step2a'
       get 'step3'
       get 'step4'
+      get 'step5'
     end
   end
 end
